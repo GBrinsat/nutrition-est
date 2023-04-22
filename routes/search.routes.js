82ee47a3e01e955eea1b48c11cb04889635c296d
Gerrit Brinsat <gerrit.brinsat@gmx.de>
2023-04-22 22:41:11 +0200

    advanced search test

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -5,6 +5,8 @@ const axios = require("axios");
 router.get("/search/test", (req, res, next) => {
     res.render("testSearch")
   })
+
+// search for specific food names:
   
 router.get("/search/foodItem", (req, res, next) => {
 
@@ -20,6 +22,28 @@ router.get("/search/foodItem", (req, res, next) => {
             res.render("searchOutput", {item : response.data.common[0]})})
         .catch(err => {next(err)})
     
+})
+
+// search for food and get its nutrients (free form text)
+
+router.post("/search/nutrients", (req, res, next) => {
+
+    const {item} = req.body
+    console.log(item)
+
+    axios({
+        method:"post",
+        url:`https://trackapi.nutritionix.com/v2/natural/nutrients`,
+        data: {
+            query: `${item}`
+        },
+        headers: {"x-app-id": "f5eefce0", "x-app-key": "4bd59f1a91c918be3c885f59ed157eec", "x-remote-user-id": "0"}
+    })
+        .then(response => {
+            console.log(response.data.foods[0])
+            res.render("searchOutput", {item : response.data.foods[0]})
+        })
+        .catch(err => {next(err)})
 })
 
   module.exports = router;
