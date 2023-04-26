9294bb97de3c433c93a6bbd262ca806d75bc5b01
Gerrit Brinsat <gerrit.brinsat@gmx.de>
2023-04-26 12:07:50 +0200

    Search improvements

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -3,24 +3,27 @@ const router = express.Router();
 const axios = require("axios");
 const User = require("../models/User.model");
 
-router.get("/search/test", (req, res, next) => {
-    res.render("testSearch")
+router.get("/search", (req, res, next) => {
+    res.render("search")
   })
 
 // search for specific food names:
   
 router.get("/search/foodItem", (req, res, next) => {
 
-    item = req.query.test
+    item = req.query.item
 
     axios({
         method:"get",
-        url:`https://trackapi.nutritionix.com/v2/search/instant?query=${item}`,
+        url:`https://trackapi.nutritionix.com/v2/search/instant?query=${item}&detailed=true`,
         headers: {"x-app-id": "f5eefce0", "x-app-key": "4bd59f1a91c918be3c885f59ed157eec", "x-remote-user-id": "0"}
     })
         .then(response => {
-            console.log(response.data.common[0])
-            res.render("searchOutput", {item : response.data.common[0]})})
+            let foodArr = []
+            foodArr = response.data.common
+            //console.log(foodArr)
+            console.log(foodArr[0].full_nutrients)
+            res.render("searchOutput", {items : foodArr})})
         .catch(err => {next(err)})
     
 })
@@ -31,7 +34,8 @@ router.post("/search/nutrients", (req, res, next) => {
 
     const user = req.session.user
     const {item} = req.body
-    console.log(item)
+    let food = null
+    let nutrientsArr = []
 
     axios({
         method:"post",
@@ -42,26 +46,31 @@ router.post("/search/nutrients", (req, res, next) => {
         headers: {"x-app-id": "f5eefce0", "x-app-key": "4bd59f1a91c918be3c885f59ed157eec", "x-remote-user-id": "0"}
     })
         .then(response => {
-            console.log(response.data.foods[0])
-            fooditem = response.data.foods[0]
-            const nutrientArr = [(response.data.foods[0].nf_calories / response.data.foods[0].serving_weight_grams) * 100, 
-                                 (response.data.foods[0].nf_total_fat / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_saturated_fat / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_cholesterol / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_sodium / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_total_carbohydrate / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_dietary_fiber / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_sugars / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_protein / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_potassium / response.data.foods[0].serving_weight_grams) * 100,
-                                 (response.data.foods[0].nf_p / response.data.foods[0].serving_weight_grams) * 100]
-            roundedNutrients = nutrientArr.map(element => Math.floor(element))
+            food = null
+            nutrientsArr = []
+            food = response.data.foods
+            //console.log(response.data.foods[0])
+            //for(let i = 0; i < 20; i++){
+/*             const nutrients =   [(response.data.foods[i].nf_calories / response.data.foods[i].serving_weight_grams) * 100, 
+                                 (response.data.foods[i].nf_total_fat / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_saturated_fat / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_cholesterol / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_sodium / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_total_carbohydrate / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_dietary_fiber / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_sugars / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_protein / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_potassium / response.data.foods[i].serving_weight_grams) * 100,
+                                 (response.data.foods[i].nf_p / response.data.foods[i].serving_weight_grams) * 100] */
+            //roundedNutrients = nutrientArr.map(element => Math.floor(element))
+            //}
             
         })
         .then(() => {
-            User.findById(user._id)
+            User.findById(user._id) 
                 .then(response => {
-                    res.render("searchOutput", {item : fooditem, nutrients : roundedNutrients, user: response})
+                    console.log(food)
+                    res.render("searchOutput", {items : food, user: response})
                 })
         })
         .catch(err => {next(err)})
